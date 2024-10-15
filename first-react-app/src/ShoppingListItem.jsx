7b4a88d51144bedae6dddfbd5da678facd6930a3
Pier-Luc Daoust <pierlucdaoust@gmail.com>
2024-10-14 21:42:04 -0400

    Mise à jour Mac - ShoppingList

---
+++ first-react-app/src/ShoppingListItem.jsx
@@ -0,0 +1,16 @@
+function ShoppingListItem({item, quantity, completed}){
+
+    const styles = {
+        color: completed ? "gray" : "red",
+        textDecoration: completed ? "line-through" : "none"
+    }
+
+return (
+    <li style={styles}>
+        {item} - {quantity}
+    </li>
+)
+};
+
+export default ShoppingListItem;
+

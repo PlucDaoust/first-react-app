79ac4fde15862c924d3097399aaf1dcd1f244c45
Pier-Luc Daoust <pierlucdaoust@gmail.com>
2024-10-14 20:51:53 -0400

    Update - dynamique component

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -2,12 +2,16 @@ import './App.css'
 import Greeter from "./Greeter";
 import Die from './Die';
 import ListPicker from './ListPicker';
+import DoubleDice from './DoubleDice';
 
 function App() {
   return (
     <>
-      <ListPicker values={[1,2,3]}/>
-      <ListPicker values={["a","b","c"]}/>
+      <DoubleDice />
+      <DoubleDice />
+      <DoubleDice />
+      {/* <ListPicker values={[1,2,3]}/>
+      <ListPicker values={["a","b","c"]}/> */}
       {/* <Die numSides={20} />
       <Die numSides={5}/>
       <Die /> */}
